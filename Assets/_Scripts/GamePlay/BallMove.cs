c03943ecdb716554a9fd083d31106c420e2964d9
KimDaeHyeon <bb0671@naver.com>
2023-09-01 13:31:01 +0900

    스크립트 위치 수정

---
+++ Assets/_Scripts/GamePlay/BallMove.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+//목적1 : 조이스틱을 당겼다가 놓으면 당긴 반대 방향으로 공이 날아가도록 한다.
+//속성1 : 조이스틱, 날아갈 방향, 공을 날리기 위한 공의 RigidBody, 날리는 힘
+//속성추가 : 조이스틱을 놓는 순간 이벤트가 발생하는데 그러면 조이스틱의 vertical, horizontal 값이 0이 되기에 조이스틱을 놓기 직전 값을 기준으로 날아가기 위해서 tempH, tempV 사용;
+//순서1-1. 조이스틱을 당긴 값을 저장한다.
+//순서1-2. 조이스틱을 놓는다.
+//순서1-3. 조이스틱을 놓기 직전 값을 기준으로 방향을 정한다.
+//순서1-4. 해당 방향을 향해 날리는 힘을 곱해서 힘을 가한다.
+
+
+//목적2 : 조이스틱을 당길 때 날아가는 방향을 표시하는 선을 그린다.
+//속성2 : 선을 그릴 LineRenderer
+//순서2-1. 조이스틱을 당긴 값으로 공이 날아갈 방향을 구한다.
+//순서2-2. 해당 방향을 LineRenderer에 넣어준다.
+
+//목적3 : 게임매니저에 현재 턴을 지정하고 자신의 턴이 아닐 때는 작동하지 않도록 한다.
+//속성3 : 내가 조작할 수 있는 턴
+//속성추가 : 자신의 턴 중에도 한번 공을 발사한 후 멈추기 전까지는 추가로 발사하지 못하도록 하기위해 isMove bool변수를 사용
+//리플레이 실행중에 실행되지 않도록 ReplayTest.replaying을 함께 사용
+//순서3-1. 내 턴이 아니면 return한다.
+
+//목적4 : 공이 충돌할 때 마다 자신의 MoveData를 게임 매니저에게 전달한다.
+//속성4 : MoveData
+
+//목적5 : 자신의 턴인 플레이어의 공이 다른 공과 부딫히면 스코어를 올린다.
+//속성5 : 스코어
+//순서5-1. 자신의 턴인 공이 다른 공과 부딫힌다.
+//순서5-2. 점수를 올린다.
+
+//목적6 : 레이를 통해서 진행 방향의 끝까지 Line을 연장해서 보여준다.
+//속성6 : 레이, 레이 히트 정보
+//순서6-1. 조이스틱을 당긴다.
+//순서6-2. 조이스틱의 방향에 따라 레이를 발사한다.
+//순서6-3. 레이가 오브젝트와 닿으면 길이를 저장해서 LineRender에게 연결해준다.
+
+public class BallMove : MonoBehaviour
+{
+    //속성1 : 조이스틱, 날아갈 방향, 공을 날리기 위한 공의 RigidBody, 날리는 힘
+    public VariableJoystick joystick;
+    public Vector3 direction;
+    protected new Rigidbody rigidbody;
+    public float power = 50;
+    //속성추가 : 조이스틱을 놓는 순간 이벤트가 발생하는데 그러면 조이스틱의 vertical, horizontal 값이 0이 되기에 조이스틱을 놓기 직전 값을 기준으로 날아가기 위해서 tempH, tempV 사용;
+    public float temph = 0;
+    public float tempv = 0;
+
+    //게스트용 추가: 호스트에게서 받아온 정보를 저장할 위치, 속도, 시간
+    public List<Vector3> positions;
+    public List<Vector3> velocities;
+    public List<float> times;
+
+    //속성2 : 선을 그릴 LineRenderer
+    protected LineRenderer lineRenderer;
+
+    //속성3 : 내가 조작할 수 있는 턴
+    public int myTurn;
+
+    //속성4 : MoveData
+    public MoveData moveData
+    {
+        get
+        {
+            if (isMove)
+            {
+                _moveData.startPos = transform.position;
+            }
+            _moveData.ballIndex = myTurn;
+            _moveData.startTime = Time.time - GameManager.Instance.shootTime;
+
+            return _moveData;
+        }
+    }
+    private MoveData _moveData = new();
+
+    //공이 움직임을 체크해서 멈춘 순간에 MoveData를 전송하기 위한 변수
+    public bool isMove = false;
+
+    //속성5 : 스코어
+    public int score;
+
+    //속성6 : 레이, 레이 히트 정보
+    protected Ray lineRay;
+    protected RaycastHit hitinfo = new();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _moveData.startPos = gameObject.transform.position;
+
+        rigidbody = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = gameObject.GetComponent<BallDoll>().showcaseColor;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+        //Debug.Log(gameObject.name + "은 " + myTurn + "에 움직인다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidbody.velocity.magnitude < 0.04f && rigidbody.velocity.magnitude != 0)
+        {
+            rigidbody.velocity *= 0.99f;
+            rigidbody.angularVelocity *= 0.99f;
+        }
+
+        if (rigidbody.velocity.magnitude > 0.04f)
+        {
+            isMove = true;
+        }
+
+        if (isMove)
+        {
+            if (rigidbody.velocity.magnitude < 0.04f)
+            {
+                if (!GuestReplayer.replaying)
+                {
+                    GameManager.Instance.ballMoveData.Add(moveData);
+                }
+                isMove = false;
+            }
+        }
+
+        //순서3-1. 내 턴이 아니면 return한다.
+        if (GameManager.Instance.turn != myTurn || !GameManager.Instance.isNobodyMove || GuestReplayer.replaying)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position);
+            return;
+        }
+
+        //순서6-1. 조이스틱을 당긴다.
+        //순서1-1. 조이스틱을 당긴 값을 저장한다.
+        float h = joystick.Horizontal;
+        float v = joystick.Vertical;
+        //조이스틱을 놓는 순간 h와 v값이 0이 되기에 두 값이 0이 아닐 경우에만 temph, tempv를 갱신한다
+        if (h != 0 || v != 0)
+        {
+            temph = h;
+            tempv = v;
+        }
+
+        //순서1-2. 조이스틱을 놓는다.     
+        //순서1-3. 조이스틱을 놓기 직전 값을 기준으로 방향을 정한다.
+        //VariableJoyStick에 구현되어 있는 OnPointerUp이 조이스틱을 떼는 순간에 작동하기에 그 함수가 실행될 때 하단에 있는 Shoot()함수를 실행시킴으로서 공을 발사한다.
+        direction = Vector3.left * temph + Vector3.back * tempv;
+
+        //순서2-1. 조이스틱을 당긴 값으로 공이 날아갈 방향을 구한다.
+        Vector3 offset = new Vector3(joystick.transform.position.x - joystick.Horizontal, 0, joystick.transform.position.y - joystick.Vertical);
+
+        //순서6-2. 조이스틱의 방향에 따라 레이를 발사한다.
+        lineRay = new Ray(transform.position, offset.normalized);
+
+        //순서6-3. 레이가 오브젝트와 닿으면 길이를 저장해서 LineRender에게 연결해준다.
+        int _layerMask = 1 << LayerMask.NameToLayer("Wall");
+        if (Physics.Raycast(lineRay, out hitinfo, 10, _layerMask))
+        {
+            //순서2-2. 해당 방향을 LineRenderer에 넣어준다.
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + offset * hitinfo.distance);
+        }
+        else
+        {
+            //순서2-2. 해당 방향을 LineRenderer에 넣어준다.
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + offset * 2);
+        }
+    }
+
+    public void Shoot()
+    {
+        rigidbody.AddForce(direction * power, ForceMode.Impulse);
+    }
+
+
+    //충돌이 발생할 시 MoveDate구조체를 게임데이터에게 쌓는다.
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!GameManager.Instance.isNobodyMove)
+        {
+            if(!GuestReplayer.replaying)
+            {
+                GameManager.Instance.ballMoveData.Add(moveData);
+
+                GetComponent<BallDoll>().CollisionEvent();
+            }
+        }
+
+        if (collision.gameObject.tag is "Player")
+        {
+            if (GameManager.Instance.turn == myTurn)
+            {
+                score++;
+
+                GameManager.Instance.UpdateScore();
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/GamePlay/GameManager.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//목적1 : 게임에 턴을 지정해서 자신의 턴인 플레이어만 조작을 할 수 있도록 한다.
+//속성1 : 현재 턴, 게임플레이어 오브젝트 집합
+//속성추가 : 공을 발사한 이후부터 공이 모두 멈추고 턴을 넘겨주는 사이에도 조작할 수 없도록 하기 위해서 bool 변수로 isNobodyMove 선언
+//리플레이 실행중에 실행되지 않도록 ReplayTest.replaying을 함께 사용
+//순서1-1. 게임 플레이어 오브젝트를 모두 찾아서 집합에 넣는다.
+//순서1-2. 집합에 순서에 따라 각 플레이어에게 턴을 배정한다.
+//순서1-3. 조이스틱에서 Shoot()을 호출했을 때 움직이는 공이 없다면 현재 플레이어에 해당하는 공의 Shoot()을 실행한다.
+//순서1-4. Shoot()을 실행한 이후에 모든 공이 멈추면 isNobodyMove값을 통해 멈췄음을 알려준다.
+
+//목적2 : 게임에 턴과 각 턴에 해당하는 플레이어를 UI로 출력한다.
+//속성2 : 현재 턴 UI, 턴 플레이어 UI
+//순서2-1. 게임 시작할 때 각 턴에 해당하는 플레이어와 현재 턴을 넣어준다.
+//순서2-2. 턴이 바뀌면 현재 턴의 값을 바꿔준다.
+
+//목적3 : 호스트에 의해서 실행된 움직임을 저장하고 게스트에게 해당 값을 보내서 리플레이 시킨다.
+//속성3 : MoveData List, 공을 발사했을 때의 시간
+
+//목적4 : 정렬된 스코어를 기준으로 플레이어와 점수를 표시한다
+//속성4 : 스코어UI
+//순서4-1. 게임 시작시 턴 순서에 따라서 0점으로 초기화된 점수를 표시한다
+//순서4-2. 점수가 바뀌면 바뀐 점수를 순위에 따라서 표시한다..
+
+//목적5 : 공의 색상을 랜덤하게 설정해준다.
+//속성5 : 색상 리스트
+
+public class GameManager : MonoBehaviour
+{
+    //싱글톤을 이용해서 쉽게 사용할 수 있도록 함
+    public static GameManager Instance;
+
+    //속성1 : 현재 턴, 게임플레이어 오브젝트 집합
+    public int turn;
+    protected GameObject[] gamePlayers;
+
+    //속성추가 : 공을 발사한 이후부터 공이 모두 멈추고 턴을 넘겨주는 사이에도 조작할 수 없도록 하기 위해서 bool 변수로 isNobodyMove 선언
+    public bool isNobodyMove = true;
+
+    //속성2 : 현재 턴 UI, 턴 플레이어 UI
+    public TMP_Text currentTurn;
+    public TMP_Text turnTable;
+
+    //속성3 : MoveData List, 공을 발사했을 때의 시간
+    public List<MoveData> ballMoveData;
+    [HideInInspector] public float shootTime;
+
+    //속성4 : 스코어UI
+    public TMP_Text scoreUI;
+
+    //속성5 : 색상 리스트
+    //public List<Material> ballColors;
+    public List<Color> ballColors;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        turn = 0;
+        //순서1-1. 게임 플레이어 오브젝트를 모두 찾아서 집합에 넣는다.
+        gamePlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        //순서1-2. 집합에 순서에 따라 각 플레이어에게 턴을 배정한다.
+        for (int i = 0; i < gamePlayers.Length; i++)
+        {
+            gamePlayers[i].GetComponent<BallMove>().myTurn = i;
+            int randomColor = Random.Range(0, ballColors.Count);
+            BallDoll ballDoll = gamePlayers[i].GetComponent<BallDoll>();
+            ballDoll.showcaseColor = ballColors[randomColor];
+            if (turn == gamePlayers[i].GetComponent<BallMove>().myTurn)
+            {
+                ballDoll.Init(ballDoll.showcaseColor, BallShowMode.MyPlayer);
+            }
+            else
+            {
+
+                ballDoll.Init(ballDoll.showcaseColor, BallShowMode.OtherPlayer);
+            }
+            ballColors.Remove(ballColors[randomColor]);
+        }
+    }
+
+    private void Start()
+    {
+        //순서2-1. 게임 시작할 때 각 턴에 해당하는 플레이어와 현재 턴을 넣어준다.
+        currentTurn.text = "Turn" + (turn + 1).ToString();
+        turnTable.text = "";
+        for (int i = 0; i < gamePlayers.Length; i++)
+        {
+            turnTable.text += "Turn " + (i + 1) + " : " + gamePlayers[i].name + "\n";
+        }
+
+        //순서4-1. 게임 시작시 턴 순서에 따라서 0점으로 초기화된 점수를 표시한다
+        scoreUI.text = "";
+        for (int i = 0; i < gamePlayers.Length; i++)
+        {
+            scoreUI.text += (i + 1).ToString() + ". " + gamePlayers[i].name + " " + gamePlayers[i].GetComponent<BallMove>().score + "\n";
+        }
+    }
+
+    public void Shoot()
+    {
+        //순서1-3. 조이스틱에서 Shoot()을 호출했을 때 움직이는 공이 없다면 현재 플레이어에 해당하는 공의 Shoot()을 실행한다.
+        if (!GuestReplayer.replaying)
+        {
+            if (isNobodyMove)
+            {
+                shootTime = Time.time;
+
+                ballMoveData.Clear();
+                for (int i = 0; i < gamePlayers.Length; i++)
+                {
+                    ballMoveData.Add(gamePlayers[i].GetComponent<BallMove>().moveData);
+                }
+
+                gamePlayers[turn].GetComponent<BallMove>().Shoot();
+                //순서1-4. Shoot()을 실행한 이후에 모든 공이 멈추면 코루틴을 통해서 isNobodyMove값을 통해 멈췄음을 알려준다.
+                StartCoroutine(EndTurn());
+            }
+            return;
+        }
+    }
+
+    //순서1-4. Shoot()을 실행한 이후에 모든 공이 멈추면 isNobodyMove값을 통해 멈췄음을 알려준다.
+    IEnumerator EndTurn()
+    {
+        //이 코루틴은 공을 쏜 후에 실행되므로 isNobodyMove를 false로 설정한다.
+        isNobodyMove = false;
+
+        //모든 공이 멈출때 까지 반복
+        while (!isNobodyMove)
+        {
+            yield return new WaitForSeconds(1f);
+            //모든 공의 속도를 측정해서 0.05보다 작다면 isNobodyMove에 true값을 그대로 넣고 속도가 0.05보다 큰 공이 있다면 isNobodyMove에 false를 넣고 break를 통해 반복문을 나온다.
+            for (int i = 0; i < gamePlayers.Length; i++)
+            {
+                isNobodyMove = true;
+                if (gamePlayers[i].GetComponent<BallMove>().isMove)
+                {
+                    //Debug.Log(gamePlayers[i].gameObject.name + gamePlayers[i].GetComponent<Rigidbody>().velocity.magnitude);
+                    isNobodyMove = false;
+                    break;
+                }
+            }
+        }
+
+        //모든 공이 움직임을 멈추고 반복문이 종료한 이후에 턴을 진행시킨다.
+        if (turn < gamePlayers.Length - 1)
+        {
+            turn++;
+        }
+        else
+        {
+            turn = 0;
+        }
+
+        for(int i = 0; i < gamePlayers.Length; i++)
+        {
+            BallDoll ballDoll = gamePlayers[i].GetComponent<BallDoll>();
+            if (turn == gamePlayers[i].GetComponent<BallMove>().myTurn)
+            {
+                ballDoll.Init(ballDoll.showcaseColor, BallShowMode.MyPlayer);
+            }
+            else
+            {
+                ballDoll.Init(ballDoll.showcaseColor, BallShowMode.OtherPlayer);
+            }
+        }
+
+        //순서2-2. 턴이 바뀌면 현재 턴의 값을 바꿔준다.
+        currentTurn.text = "Turn" + (turn + 1).ToString();
+    }
+
+    //순서4-2. 점수가 바뀌면 바뀐 점수를 순위에 따라서 표시한다.
+    public void UpdateScore()
+    {
+        //플레이어명과 스코어를 배열로 저장
+        string[] playerName = new string[gamePlayers.Length];
+        int[] scores = new int[gamePlayers.Length];
+        for (int i = 0; i < gamePlayers.Length; i++)
+        {
+            playerName[i] = gamePlayers[i].name;
+            scores[i] = gamePlayers[i].GetComponent<BallMove>().score;
+        }
+
+        //배열을 점수 오름차순으로 정렬
+        for (int i = 0; i < gamePlayers.Length; i++)
+        {
+            for (int j = 0; j < gamePlayers.Length - (i + 1); j++)
+            {
+                if (scores[j] < scores[j + 1])
+                {
+                    int temp = scores[j + 1];
+                    scores[j + 1] = scores[j];
+                    scores[j] = temp;
+                    string tempText = playerName[j + 1];
+                    playerName[j + 1] = playerName[j];
+                    playerName[j] = tempText;
+                }
+                else if (scores[j] == scores[j + 1])
+                {
+                    if (gamePlayers[j].GetComponent<BallMove>().myTurn > gamePlayers[j + 1].GetComponent<BallMove>().myTurn)
+                    {
+                        int temp = scores[j + 1];
+                        scores[j + 1] = scores[j];
+                        scores[j] = temp;
+                        string tempText = playerName[j + 1];
+                        playerName[j + 1] = playerName[j];
+                        playerName[j] = tempText;
+                    }
+                }
+            }
+        }
+
+        scoreUI.text = "";
+        //배열을 순서대로 점수로 출력
+        for (int i = 0; i < gamePlayers.Length; i++)
+        {
+            scoreUI.text += (i + 1).ToString() + ". " + playerName[i] + " " + scores[i] + "\n";
+        }
+    }
+}
