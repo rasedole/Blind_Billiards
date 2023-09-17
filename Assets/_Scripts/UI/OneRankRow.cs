a19923d9b9bd2a40770ac8295fddc6cd7c56244a
seintcat <1x2x0x7@naver.com>
2023-09-17 14:33:20 +0900

    랭킹에 id 추가하면서 랭킹 표시 텍스트를 불러오지 못하는 오류 수정중

---
+++ Assets/_Scripts/UI/OneRankRow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class OneRankRow : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI num;
+    [SerializeField]
+    private TextMeshProUGUI id;
+    [SerializeField]
+    private TextMeshProUGUI date;
+    [SerializeField]
+    private TextMeshProUGUI score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Scripts/UI/RankingResultUI.cs
+++ Assets/_Scripts/UI/RankingResultUI.cs
@@ -18,11 +18,15 @@ public class RankingResultUI : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
+    private bool uiShow;
+
     void Awake()
     {
         if (null == instance)
         {
             instance = this;
+            uiShow = false;
+            savedRankDatas = new List<RankData>();
         }
     }
 
@@ -34,8 +38,8 @@ public class RankingResultUI : MonoBehaviour
 
     public void RankShow(List<RankData> rankDatas)
     {
+        savedRankDatas.Clear();
         LoadRankData(rankDatas.Count);
-        maxRankingRow = rankDatas.Count;
 
         // 새 점수 추가
         foreach (RankData rankData in rankDatas)
@@ -47,28 +51,33 @@ public class RankingResultUI : MonoBehaviour
         savedRankDatas = savedRankDatas.OrderByDescending(x => x.score).ToList();
 
         // maxRankingRow 밖의 순위는 삭제
-        if(maxRankingRow < savedRankDatas.Count)
+        if (maxRankingRow < savedRankDatas.Count)
         {
-            savedRankDatas.RemoveRange(maxRankingRow, savedRankDatas.Count-maxRankingRow);
+            savedRankDatas.RemoveRange(maxRankingRow, savedRankDatas.Count - maxRankingRow);
         }
 
+        //Debug.LogWarning(savedRankDatas.Count + ", " + maxRankingRow);
         if (savedRankDatas.Count < maxRankingRow)
             maxRankingRow = savedRankDatas.Count;
-
-        // 화면에 보여주기.
-        ShowRankData();
 
         // 변경된 순위 데이터 저장
         SaveRankData(rankDatas.Count);
 
-        Invoke("ResetUI", 6f);
+        // 화면에 보여주기.
+        ShowRankData();
+
+        if (TCP_BallCore.networkMode != NetworkMode.Server)
+        {
+            Invoke("ResetUI", 6f);
+        }
     }
 
     // 게임한 인원수(1p/2p/..)에 해당하는 저장된 랭킹 데이터 받아오기
     void LoadRankData(int headCount)
     {
         RankData rankData = new RankData();
-        for (int i = 1; i <= maxRankingRow; i++)
+        maxRankingRow = rankObj.Length;
+        for (int i = 0; i < maxRankingRow; i++)
         {
             // 저장 key 형식예시 : Rank 1P 1 playtime
             //         형식     : Rank | 플레이어수 | 랭킹(order) | playTime/score
@@ -98,17 +107,17 @@ public class RankingResultUI : MonoBehaviour
             obj.SetActive(true);
 
             // 0번째 자식 == 순위
-            obj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = rankCount.ToString();
+            obj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (rankCount + 1).ToString();
 
-            obj.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = savedRankDatas[rankCount - 1].id;
+            obj.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = savedRankDatas[rankCount].id;
 
             // 1번째 자식 == 게임 시간
-            string date = savedRankDatas[rankCount - 1].playTime.ToString("yyyy-MM-dd") +
-                "\n" + savedRankDatas[rankCount - 1].playTime.ToString("HH:mm");
+            string date = savedRankDatas[rankCount].playTime.ToString("yyyy-MM-dd") +
+                "\n" + savedRankDatas[rankCount].playTime.ToString("HH:mm");
             obj.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = date;
 
             // 2번째 자식 == 점수
-            obj.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = savedRankDatas[rankCount - 1].score.ToString();
+            obj.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = savedRankDatas[rankCount].score.ToString();
 
             rankCount++;
         }
@@ -118,22 +127,34 @@ public class RankingResultUI : MonoBehaviour
     // savedRankDatas PlayerPrefs 저장
     void SaveRankData(int headCount)
     {
-        for (int i = 1; i <= maxRankingRow; i++)
+        maxRankingRow = rankObj.Length;
+        for (int i = 0; i < maxRankingRow; i++)
         {
-            PlayerPrefs.SetString("Rank " + headCount + "P " + i + "playTime", savedRankDatas[i - 1].playTime.ToString());
-            PlayerPrefs.SetString("Rank " + headCount + "P " + i + "id", savedRankDatas[i - 1].id);
-            PlayerPrefs.SetString("Rank " + headCount + "P " + i + "score", savedRankDatas[i - 1].score.ToString());
+            if (savedRankDatas.Count <= i)
+            {
+                Debug.LogWarning("out!");
+                break;
+            }
+            PlayerPrefs.SetString("Rank " + headCount + "P " + i + "playTime", savedRankDatas[i].playTime.ToString());
+            PlayerPrefs.SetString("Rank " + headCount + "P " + i + "id", savedRankDatas[i].id);
+            PlayerPrefs.SetString("Rank " + headCount + "P " + i + "score", savedRankDatas[i].score.ToString());
         }
+        //Debug.LogWarning(savedRankDatas.Count + ", !" + maxRankingRow);
     }
 
     public static void StartRankUI(List<RankData> rankDatas)
     {
-        instance.RankShow(rankDatas);
+        if (!instance.uiShow)
+        {
+            instance.uiShow = true;
+            instance.RankShow(rankDatas);
+        }
     }
 
     private void ResetUI()
     {
         animator.Play("Out");
+        uiShow = false;
     }
     //public void UpdateRankData(List<RankData> rankDatas)
     //{
